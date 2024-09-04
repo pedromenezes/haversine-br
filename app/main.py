e4cd95db80dfefc13d50b0e6088d8d1e127618d0
Pedro Menezes <pedrojudo+github@gmail.com>
2024-09-04 10:11:28 -0300

    lint

--- app/main.py
+++ app/main.py
@@ -13,33 +13,50 @@ app = FastAPI()
 redis_host = os.getenv('REDIS_HOST', 'redis')
 redis_client = redis.StrictRedis(host=redis_host, port=6379, db=0)
 
+
 class NearestCity(BaseModel):
     name: str
     distance: float
+
 
 class NearestCitiesResponse(BaseModel):
     city: str
     nearest_cities: List[NearestCity]
     unit: str = "km"
 
+
 @app.get("/nearest_cities/{city_name}", response_model=NearestCitiesResponse)
-def get_nearest_cities(city_name: str, max_results: Optional[int] = Query(5, gt=0, le=10)):
+def get_nearest_cities(
+    city_name: str,
+    max_results: Optional[int] = Query(5, gt=0, le=10)
+):
     """
     Retorna as cidades mais próximas em quilômetros.
-    
+
     - **city_name**: Nome da cidade para a qual encontrar as cidades mais próximas.
     - **max_results**: Número máximo de resultados a serem retornados (1-10, padrão: 5).
     """
     hash_key = f"distances:{city_name}"
     if not redis_client.exists(hash_key):
         raise HTTPException(status_code=404, detail="City not found")
-    
+
     distances = redis_client.hgetall(hash_key)
-    nearest_cities = [{"name": city.decode("utf-8"), "distance": float(distance)} for city, distance in distances.items()]
+    nearest_cities = [
+        {"name": city.decode("utf-8"), "distance": float(distance)}
+        for city, distance in distances.items()
+    ]
     nearest_cities.sort(key=lambda x: x["distance"])
 
-    return {"city": city_name, "nearest_cities": nearest_cities[:max_results], "unit": "km"}
+    return {
+        "city": city_name,
+        "nearest_cities": nearest_cities[:max_results],
+        "unit": "km"
+    }
+
 
 @app.get("/")
 def read_root():
-    return {"message": "Bem-vindo à API de Distância entre Cidades. Use /docs para ver os endpoints disponíveis."}
+    return {
+        "message": """Bem-vindo à API de Distância entre Cidades.
+        Use /docs para ver os endpoints disponíveis."""
+    }
--- scripts/process_data.py
+++ scripts/process_data.py
@@ -15,6 +15,7 @@ response = httpx.get(url)
 logger.info("Arquivo JSON baixado com sucesso")
 cities = response.json()
 
+
 def calculate_distances(city, cities):
     city_name = city["nome"]
     city_lat_lon = (city["latitude"], city["longitude"])
@@ -23,26 +24,36 @@ def calculate_distances(city, cities):
         other_city_name = other_city["nome"]
         if city_name != other_city_name:
             other_city_lat_lon = (other_city["latitude"], other_city["longitude"])
-            distance = haversine(city_lat_lon, other_city_lat_lon)  # Distâncias em quilômetros
+            distance = haversine(city_lat_lon, other_city_lat_lon)  # Distâncias em km
             distances.append({"name": other_city_name, "distance": distance})
     distances.sort(key=lambda x: x["distance"])
     return city_name, distances[:5]
+
 
 def save_results_to_redis(city_name, distances):
     hash_key = f"distances:{city_name}"
     for distance in distances:
         redis_client.hset(hash_key, distance["name"], distance["distance"])
-    logger.info(f"Distâncias salvas no Redis para a cidade {city_name}")
+    # logger.info(f"Distâncias salvas no Redis para a cidade {city_name}")
+
 
 def process_city(city, cities):
     city_name, distances = calculate_distances(city, cities)
     save_results_to_redis(city_name, distances)
 
+
 logger.info("Calculando distâncias entre as cidades")
 for i, city in enumerate(cities):
     if not redis_client.exists(f"distances:{city['nome']}"):
         process_city(city, cities)
-    if (i + 1) % (len(cities) // 20) == 0 or (i + 1) == len(cities):  # Log a cada 5% ou ao final
-        logger.info(f"Progresso: {i + 1}/{len(cities)} cidades processadas ({((i + 1) / len(cities)) * 100:.1f}%)")
+    # Registra o progresso a cada 5% ou ao final
+    total_cities = len(cities)
+    progress_interval = total_cities // 20  # 5% do total
+    if (i + 1) % progress_interval == 0 or i + 1 == total_cities:
+        progress = (i + 1) / len(cities) * 100
+        logger.info(
+            f"Progresso: {i + 1}/{len(cities)} cidades processadas ({progress:.1f}%)"
+        )
+
 
 logger.info("Distâncias calculadas com sucesso")
